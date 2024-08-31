98e88a93105a46eb99b157fce97997049f8d24ab
Maicon Cezar <43833424+MMCAC@users.noreply.github.com>
2024-08-31 19:15:54 -0300

    add do files

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { UserContext } from '../../context/UserContext'
+import {Navigate} from 'react-router-dom'
+
+type Props = {
+  children: JSX.Element
+}
+
+const ProtectedRoute = ({children}: Props) => {
+  const {isSessionValid} = useContext(UserContext)
+
+  if(isSessionValid()) {
+    return children
+  }
+
+  return <Navigate to='/' replace/>
+}
+
+export default ProtectedRoute
